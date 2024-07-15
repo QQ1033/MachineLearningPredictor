73f4f3ad4798055ffe1cae36d2c85ee3e53f5dc0
Aiden Loza <aidenloza@gmail.com>
2024-07-15 15:16:34 -0700

    Feature functions

---
+++ stockLearner.py
@@ -0,0 +1,126 @@
+import csv
+
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+
+# TODO complete these functions
+
+def func1(s):
+    return len(s.split()) / (len(s) - s.count(' '))
+
+def func2(s):
+    count  = 0
+    for char in s:
+        if (s.isupper()):
+            count += 1
+
+    return count
+
+
+def func3(s):
+    if (s.contains('%')):
+        return 1.0
+
+    return 0.0
+
+def func4(s):
+    if (s.split.contains("Dow Jones")):
+        return 10.0
+    if (s.split.contains("DJIA")):
+        return 10.0
+    if (s.split.contains("DJIA")):
+        return 10.0
+
+    return 0.0
+
+
+def func5(s):
+    if (s.split.contains("Conflict")):
+        return -5.0
+    if (s.split.contains("War")):
+        return -5.0
+
+    return 0.0
+
+
+def func6(s):
+    count = 0
+    for char in s:
+        if (s.isdigit()):
+            count += 1
+
+    return count/ (len(s) - s.count(' '))
+
+
+
+def func7(s):
+    count = 0
+    for string in s:
+        if (string.tolowercase.contains("politics") or string.tolowercase.contains("politician")
+                or string.tolowercase.contains("leader") or string.tolowercase.contains("president")):
+            count += 1
+
+    return count
+
+
+def func8(s):
+    if (s.contains('?')):
+        return True
+
+    return False
+
+def func9(s):
+    count = 0
+    for string in s:
+        if (string.tolowercase.contains("and") or string.tolowercase.contains("or")
+                or string.tolowercase.contains("the") or string.tolowercase.contains("for")):
+            count += 1
+
+    return count
+
+def func10(s):
+    return s.length
+
+def get_data():
+    with open('Combined_News_DJIA.csv') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+
+        # skip header but get num_headlines by ignoring first two columns
+        num_headlines = len(next(csv_reader)) - 2
+        inputs = []
+        targets = []
+        feature_funcs = [
+            func1, func2, func3, func4, func5,
+            func6, func7, func8, func9, func10
+        ]
+
+        for line in csv_reader:
+            targets.append(int(line[1]))
+            inp_row = []
+            headlines = line[2:]
+            for headline in headlines:
+                for func in feature_funcs:
+                    inp_row.append(func(headline))
+            # A few rows are short of having the necessary number of headlines;
+            # append zeros if this row is short
+            inp_row += (num_headlines - len(headlines)) * len(feature_funcs) * [0]
+            inputs.append(inp_row)
+
+    inputs = np.array(inputs)
+    targets = np.array(targets)
+    return inputs, targets
+
+def main():
+    inputs, targets = get_data()
+    classifier = MLPClassifier(random_state=0, verbose=0)
+    test_size = 10
+    classifier.fit(inputs[test_size:], targets[test_size:])
+    predictions = classifier.predict(inputs[:test_size])
+    print(f'{predictions = }')
+    print(f'{targets[:test_size] = }')
+
+if __name__ == '__main__':
+    main()
+
+
